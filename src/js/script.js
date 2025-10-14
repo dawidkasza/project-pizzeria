cbd18925be98e93d812b98ecff06ec9194f7246d
Dawid Kasza <dawidkasza05@gmail.com>
2025-10-14 18:35:04 +0200

    Add AJAX

--- src/js/script.js
+++ src/js/script.js
@@ -71,7 +71,11 @@ const settings = {
   cart: {
     defaultDeliveryFee: 20,
   },
-
+  db: {
+  url: '//localhost:3131',
+  products: 'products',
+  orders: 'orders',
+},
 };
 
 const templates = {
@@ -441,13 +445,25 @@ const templates = {
     initMenu: function(){
       const thisApp = this;
       for (let productData in thisApp.data.products){
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function(){
       const thisApp = this;
-      thisApp.data = dataSource;
+      thisApp.data = {};
+      const url = settings.db.url + '/' + settings.db.products;
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+          thisApp.data.products = parsedResponse;
+          thisApp.initMenu();
+        });
+
+      console.log('ThisApp.data', JSON.stringify(thisApp.data));
     },
 
     initCart: function(){
@@ -464,7 +480,6 @@ const templates = {
       console.log('settings:', settings);
       console.log('templates:', templates);
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
   };
