937bb86d9b692ec0f9109c3f1531c5d55c19311a
Dawid Kasza <dawidkasza05@gmail.com>
2025-10-19 13:44:34 +0200

    Add new class Booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import {settings, select, classNames} from './setting.js'; 
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Bokking.js';
 
 const app = {
   initPages(){
@@ -79,6 +80,12 @@ const app = {
     });
   },
 
+  initBooking: function(){
+    const thisApp = this;
+    const bookingElem = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingElem);
+  },
+
   init: function(){
     const thisApp = this;
     //console.log('*** App starting ***');
@@ -89,6 +96,7 @@ const app = {
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
