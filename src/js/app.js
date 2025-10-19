f19fc674cf0be98f89ba07fb9fe9868d9250aa9f
Dawid Kasza <dawidkasza05@gmail.com>
2025-10-19 13:05:20 +0200

    Add changes between pages

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,52 @@
-import {settings, select} from './setting.js'; 
+import {settings, select, classNames} from './setting.js'; 
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages(){
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = thisApp.pages[0].id;
+    for(let page of thisApp.pages){
+      if (page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#','');
+        /* run thisApp.activatePage with that id*/
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      })
+    }
+  },
+
+  activatePage(pageId){
+    const thisApp = this;
+    /* add class "active" to maching pages, remove from non-matching */
+    for (let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id==pageId);
+    }
+    /* add class "active" to maching links, remove from non-matching */
+    for (let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href')=='#'+pageId
+      );
+    }
+  },
+
   initMenu: function(){
     const thisApp = this;
     for (let productData in thisApp.data.products){
@@ -19,11 +63,9 @@ const app = {
         return rawResponse.json();
       })
       .then(function(parsedResponse){
-        console.log('parsedResponse', parsedResponse);
         thisApp.data.products = parsedResponse;
         thisApp.initMenu();
       });
-      console.log('ThisApp.data', JSON.stringify(thisApp.data));
   },
 
   initCart: function(){
@@ -44,6 +86,7 @@ const app = {
     //console.log('classNames:', classNames);
     //console.log('settings:', settings);
     //console.log('templates:', templates);
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
